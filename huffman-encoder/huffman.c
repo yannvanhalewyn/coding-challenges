2ec0e0483d8a2ee1b243e98244f826ceb194c305
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2026-02-07 22:42:18 +0100

    (feat): check for file existance

--- huffman-encoder/huffman.c
+++ huffman-encoder/huffman.c
@@ -1,19 +1,28 @@
 #include <stdio.h>
+#define true 1
+#define false 0
 
 int file_exists(const char *filename) {
-  FILE *file = fopen(filename, "r");
-  if (file) {
-    fclose(file);
-    return 1;
-  }
-  return 0;
+    FILE *file = fopen(filename, "r");
+    if (file) {
+        fclose(file);
+        return true;
+    }
+    return false;
 }
 
 int main(int argc, char *argv[]) {
-  if (argc < 2) {
-    fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
-    return 1;
-  }
-  printf("Hello %s", argv[1]);
-  return 0;
+    if (argc < 2) {
+        fprintf(stderr, "Usage: %s <filename>\n", argv[0]);
+        return 1;
+    }
+    char* filename = argv[1];
+
+    if (file_exists(filename)) {
+        printf("exists! %s", filename);
+    }
+    else {
+        printf("FIle does not exist: %s", filename);
+    }
+    return 0;
 }
